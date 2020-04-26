67d932620a445617f92f5cd228246797949daa0d
MattShilling <shillinm@oregonstate.edu>
2020-04-25 22:19:13 -0700

    Adding bash script to run a variety of tests over # nodes and # threads.

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+./Superquad 8 64 varying_nodes.csv
+./Superquad 8 128 varying_nodes.csv
+./Superquad 8 256 varying_nodes.csv
+./Superquad 8 512 varying_nodes.csv
+./Superquad 8 1024 varying_nodes.csv
+./Superquad 8 2048 varying_nodes.csv
+./Superquad 8 4096 varying_nodes.csv
+./Superquad 8 8192 varying_nodes.csv
+./Superquad 8 16384 varying_nodes.csv
+./Superquad 8 32768 varying_nodes.csv
+
+# ./Superquad 1 2048 varying_threads.csv
+# ./Superquad 2 2048 varying_threads.csv
+# ./Superquad 3 2048 varying_threads.csv
+# ./Superquad 4 2048 varying_threads.csv
+# ./Superquad 5 2048 varying_threads.csv
+# ./Superquad 6 2048 varying_threads.csv
+# ./Superquad 7 2048 varying_threads.csv
+# ./Superquad 8 2048 varying_threads.csv
+# ./Superquad 9 2048 varying_threads.csv
+# ./Superquad 10 2048 varying_threads.csv
+# ./Superquad 11 2048 varying_threads.csv
+# ./Superquad 12 2048 varying_threads.csv
+
+
+
+
+
+
