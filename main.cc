078e5a47a8cf43ff3e34537c25ebd625c14e12f0
MattShilling <shillinm@oregonstate.edu>
2020-04-26 00:09:48 -0700

    Playing around with formulas to see if I can fix the 'diminishing volumes' problem.

--- main.cc
+++ main.cc
@@ -35,7 +35,7 @@ float Height(int, int, int);
 struct test_mem {
   int numnodes;
   float volume;
-} ;
+};
 
 void calc_superquad(std::shared_ptr<void> mem) {
 
@@ -43,9 +43,11 @@ void calc_superquad(std::shared_ptr<void> mem) {
   test_mem *data = static_cast<test_mem *>(mem.get());
   const int numnodes = data->numnodes;
 
-  // The area of a single full-sized tile: dA.
-  const float dA = (((XMAX - XMIN) / (float)(numnodes - 1)) *
-                    ((YMAX - YMIN) / (float)(numnodes - 1)));
+  // The area of a single full-sized tile: dA. NOTE: There should be no
+  // "numnodes-1" term because the total number of nodes is numnodes^2, not
+  // (numnodes-1)^2.
+  const float dA =
+      (XMAX - XMIN) * (YMAX - YMIN) / pow(static_cast<float>(numnodes), 2.0);
 
   // Use an OpenMP for loop and a reduction to find weighted heights.
   float total_height = 0.0;
@@ -54,7 +56,8 @@ void calc_superquad(std::shared_ptr<void> mem) {
   for (int iv = 0; iv < numnodes; iv++) {
     for (int iu = 0; iu < numnodes; iu++) {
       float weight = 0.0;
-      // Logic to decide if we are on a side of the superquadric or not. If both are true then we are on a corner.
+      // Logic to decide if we are on a side of the superquadric or not. If both
+      // are true then we are on a corner.
       const bool side_iv = (iv == numnodes - 1) || (iv == 0);
       const bool side_iu = (iu == numnodes - 1) || (iu == 0);
       // XOR
@@ -69,7 +72,7 @@ void calc_superquad(std::shared_ptr<void> mem) {
         weight = 1.0;
       }
 
-      total_height += weight * Height(iu, iv, numnodes);
+      total_height += Height(iu, iv, numnodes);
     }
   }
 
@@ -84,6 +87,7 @@ int main(int argc, char *argv[]) {
   // Taking in some command line arguments to control the program.
   int threads = NUMT;
   int numnodes = NUMNODES;
+  bool just_volume = false;
   std::string file;
 
   if (argc >= 2) {
@@ -95,7 +99,15 @@ int main(int argc, char *argv[]) {
   }
 
   if (argc >= 4) {
-    file = std::string(argv[3]);
+    std::string temp(argv[3]);
+    if (temp == "-vol") {
+      if (argc >= 5) {
+        file = std::string(argv[4]);
+        just_volume = true;
+      }
+    } else {
+      file = std::string(argv[3]);
+    }
   }
 
   std::shared_ptr<test_mem> mem = std::make_shared<test_mem>();
@@ -105,43 +117,70 @@ int main(int argc, char *argv[]) {
   TestRig superquad(mem, calc_superquad,
                     std::function<void(std::shared_ptr<void> mem)>());
 
-  superquad.Init(1);
-  superquad.Run(static_cast<double>(numnodes * numnodes));
-  float superquad_one = superquad.MaxMegaMults();
+  std::cout << 1.00 / 32768.00 << std::endl;
 
   superquad.Init(threads);
   superquad.Run(static_cast<double>(numnodes * numnodes));
-  float superquad_two = superquad.MaxMegaMults();
-
-  double speedup = CalcSpeedup(superquad_two, superquad_one);
-  float fp = CalcFp(speedup, threads);
 
   if (!file.empty()) {
+    float superquad_two = 0.0;
+    float superquad_one = 0.0;
+    double speedup = 0.0;
+    float fp = 0.0;
+
+    if (!just_volume) {
+      // We only need to run a test with one thread if we want to find the
+      // speedup and parallel fraction.
+      superquad_two = superquad.MaxMegaMults();
+
+      superquad.Init(1);
+      superquad.Run(static_cast<double>(numnodes * numnodes));
+      superquad_one = superquad.MaxMegaMults();
+
+      speedup = CalcSpeedup(superquad_two, superquad_one);
+      fp = CalcFp(speedup, threads);
+    }
+
     std::ofstream outfile;
     outfile.open(file, std::ios_base::app);
 
-    std::cout << std::fixed << std::setprecision(6) << "Writing: \n" << threads
-              << '\t' << numnodes << '\t' << superquad_two << '\t' << speedup
-              << '\t' << fp << "\nto " << file << std::endl;
-    // Setting the precision for output.
+    // Setting the proper precision.
     outfile << std::fixed;
     outfile << std::setprecision(3);
-    outfile << threads << '\t' << numnodes << '\t' << mem->volume << '\t' << superquad_two << '\t'
-            << speedup << '\t' << fp << std::endl;
+    std::cout << std::fixed;
+    std::cout << std::setprecision(3);
+
+    if (!just_volume) {
+      // We care about all the data.
+      std::cout << "Writing: \n" << threads << '\t' << numnodes << '\t'
+                << superquad_two << '\t' << speedup << '\t' << fp << "\nto "
+                << file << std::endl;
+
+      outfile << threads << '\t' << numnodes << '\t' << mem->volume << '\t'
+              << superquad_two << '\t' << speedup << '\t' << fp << std::endl;
+    } else {
+      // We only care about the volume results.
+      std::cout << "Writing: \n" << threads << '\t' << numnodes << '\t'
+                << mem->volume << "\nto " << file << std::endl;
+
+      outfile << threads << '\t' << numnodes << '\t' << mem->volume
+              << std::endl;
+    }
     outfile.close();
   }
 }
 
 // iu,iv = 0 .. NUMNODES-1
 float Height(int iu, int iv, int numnodes) {
-  float x = -1. + 2. * (float)iu / (float)(numnodes - 1); // -1. to +1.
-  float y = -1. + 2. * (float)iv / (float)(numnodes - 1); // -1. to +1.
 
-  float xn = pow(fabs(x), (double)N);
-  float yn = pow(fabs(y), (double)N);
-  float r = 1. - xn - yn;
-  if (r < 0.)
-    return 0.;
-  float height = pow(1. - xn - yn, 1. / (float)N);
+  double x = -1.00 + 2.00 * ((double)iu / (double)(numnodes - 1)); // -1. to +1.
+  double y = -1.00 + 2.00 * ((double)iv / (double)(numnodes - 1)); // -1. to +1.
+
+  double xn = pow(fabs(x), (double)N);
+  double yn = pow(fabs(y), (double)N);
+  double r = 1.00 - xn - yn;
+  if (r < 0.00)
+    return 0.00;
+  float height = pow(r, 1.00 / (float)N);
   return height;
 }
